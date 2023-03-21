8a146036ee27e7310f2f10a4384a76250af4f1b0
seungyeonnnnn <fkdltldls@gmail.com>
2023-03-22 00:02:53 +0900

    join, login

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -23,7 +23,7 @@ export const watch = async (req, res) => {
   const { id } = req.params; //ES6 Version
   const video = await Video.findById(id);
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   return res.render("watch", { pageTitle: video.title, video });
 };
@@ -32,7 +32,7 @@ export const getEdit = async (req, res) => {
   const { id } = req.params; //ES6 Version
   const video = await Video.findById(id);
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
@@ -66,7 +66,7 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect("/");
   } catch (error) {
-    return res.render("upload", {
+    return res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
@@ -11,7 +11,7 @@ app.set("view engine", "pug"); // >npm i pug 설치 후 추가
 app.set("views", process.cwd() + "/src/views"); // views directories 위치 이동
 app.use(logger);
 app.use(express.urlencoded({ extended: true })); //express app이 form의 value들을 이해할 수 있도록 함 & JS 형식으로 변형
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
