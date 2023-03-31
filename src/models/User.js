65b53606c8e253e02837e6d293ba34d7d9d8b6e2
seungyeonnnnn <fkdltldls@gmail.com>
2023-04-01 00:25:39 +0900

    User Authentication

--- src/models/User.js
+++ src/models/User.js
@@ -3,9 +3,11 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  avatarUrl: { type: String },
+  socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  name: { type: String, required: true },
+  password: { type: String },
+  name: { type: String },
   location: String,
 });
 
--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -11,6 +15,22 @@ app.set("view engine", "pug"); // >npm i pug 설치 후 추가
 app.set("views", process.cwd() + "/src/views"); // views directories 위치 이동
 app.use(logger);
 app.use(express.urlencoded({ extended: true })); //express app이 form의 value들을 이해할 수 있도록 함 & JS 형식으로 변형
+
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false, // Login한 브라우저에만 Cookie를 전달하고 Session을 저장.
+    // postLogin에서 Session을 수정하기 때문에 Login할 경우에만 저장하게됨
+    // // 해당 시간동안 cookie가 유지되고 지나면 만료되어 Logout됨
+    // cookie: {
+    //   maxAge: 20000,
+    // },
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
